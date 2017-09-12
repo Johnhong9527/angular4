338fa25eba372407954cad954a3083cd9f2344cc
seam <honghaitzz11@gmail.com>
2017-09-12 16:53:47 +0800

    表单验证

--- src/app/suk/directive.ts
+++ src/app/suk/directive.ts
@@ -1,9 +1,17 @@
-import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, Validators } from '@angular/forms';
+import {Directive, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, Validators} from '@angular/forms';
 
-/** A hero's name can't match the given regular expression */
+/** 英雄的名字不能与给定的正则表达式匹配 */
+
+/*
+这个函数实际上是一个工厂，它接受一个用来检测指定名字是否已被禁用的正则表达式，并返回一个验证器函数。
+
+在本例中，禁止的名字是“bob”； 验证器会拒绝任何带有“bob”的英雄名字。 在其他地方，只要配置的正则表达式可以匹配上，它可能拒绝“alice”或者任何其他名字。
+
+forbiddenNameValidator工厂函数返回配置好的验证器函数。 该函数接受一个Angular控制器对象，并在控制器值有效时返回null，或无效时返回验证错误对象。 验证错误对象通常有一个名为验证秘钥（forbiddenName）的属性。其值为一个任意词典，我们可以用来插入错误信息（{name}）。
+*/
 export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
-  return (control: AbstractControl): {[key: string]: any} => {
+  return (control: AbstractControl): { [key: string]: any } => {
     const forbidden = nameRe.test(control.value);
     return forbidden ? {'forbiddenName': {value: control.value}} : null;
   };
@@ -16,12 +24,11 @@ export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
 export class ForbiddenValidatorDirective implements Validator {
   @Input() forbiddenName: string;
 
-  validate(control: AbstractControl): {[key: string]: any} {
+  validate(control: AbstractControl): { [key: string]: any } {
     return this.forbiddenName ? forbiddenNameValidator(new RegExp(this.forbiddenName, 'i'))(control)
       : null;
   }
 }
-
 
 
 /*
