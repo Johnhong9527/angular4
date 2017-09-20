ab17bc3e74f4c5c5851a57a37a7b501e61b267a7
seam <honghaitzz11@gmail.com>
2017-09-20 15:57:17 +0800

    httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {HttpModule} from "@angular/http";
 import {AppComponent} from './app.component';
 import { SimpleHttpComponent } from './simple-http/simple-http.component';
 import { MyHttpComponent } from './my-http/my-http.component';
+import { HttpClientComponent } from './http-client/http-client.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleHttpComponent,
-    MyHttpComponent
+    MyHttpComponent,
+    HttpClientComponent
   ],
   imports: [
     BrowserModule,
