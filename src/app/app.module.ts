15cc345a16a95bce2125b1a4cd3d1383c13f3425
seam <honghaitzz11@gmail.com>
2017-09-25 13:32:07 +0800

    基本路由使用方式

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterComponent} from './router/router.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import {APP_BASE_HREF} from "@angular/common";
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },  // 默认首页设置
+  { path: 'home', component: HomeComponent }, // 首页模块
+  { path: 'about', component: AboutComponent }, // 关于模块
+  { path: 'contact', component: ContactComponent }, // 文章内容
+  { path: 'contactus', redirectTo: 'contact' }, // 所有文章内容
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
