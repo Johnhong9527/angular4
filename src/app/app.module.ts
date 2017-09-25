8b4f77170bb5f45073cf8de766a939fc019ab9a6
seam <honghaitzz11@gmail.com>
2017-09-25 13:37:11 +0800

    0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {RouterComponent} from './router/router.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import {APP_BASE_HREF} from "@angular/common";
+import {AboutComponent} from './about/about.component';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+import {APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from "@angular/common";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },  // 默认首页设置
-  { path: 'home', component: HomeComponent }, // 首页模块
-  { path: 'about', component: AboutComponent }, // 关于模块
-  { path: 'contact', component: ContactComponent }, // 文章内容
-  { path: 'contactus', redirectTo: 'contact' }, // 所有文章内容
+  {path: '', redirectTo: 'home', pathMatch: 'full'},  // 默认首页设置
+  {path: 'home', component: HomeComponent}, // 首页模块
+  {path: 'about', component: AboutComponent}, // 关于模块
+  {path: 'contact', component: ContactComponent}, // 文章内容
+  {path: 'contactus', redirectTo: 'contact'}, // 所有文章内容
 ];
 
 
@@ -31,7 +31,8 @@ const routes: Routes = [
     RouterModule.forRoot(routes)
   ],
   providers: [
-    { provide: APP_BASE_HREF, useValue: '/' }
+    {provide: APP_BASE_HREF, useValue: '/'},
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
   ],
   bootstrap: [AppComponent]
 })
